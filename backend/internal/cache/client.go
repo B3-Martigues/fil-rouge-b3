11c5a5c0bf8dffe9fb309e9f5d7c48015666b466
unknown <younes.kribeche@laplateforme.io>
2026-07-13 02:16:51 +0200

    feat(deployment): CI improved

--- backend/internal/cache/client.go
+++ backend/internal/cache/client.go
@@ -13,6 +13,7 @@ import (
 type Client struct {
 	*redis.Client
 }
+
 // New initialise un client Redis à partir de la configuration de
 // l'application et vérifie immédiatement la connexion avec un PING.
 // Une erreur est renvoyée si le serveur Redis n'est pas accessible.
